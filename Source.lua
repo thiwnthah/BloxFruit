85e64d53e078a02cdcbbe939b2d4551c32d5ea2e
thiwnthah <phamngocthienthanh207@gmail.com>
2025-03-27 18:30:09 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -3,30 +3,30 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        local Window = MakeWindow({
          Hub = {
          Title = "THIWN THAH HUB",
-         Animation = "BY: THIÊN THANH OFFICIAL"
+         Animation = "THIWN THAH HUB"
          },
         Key = {
         KeySystem = true,
-        Title = "Key System",
-        Description = "GEYKEY",
+        Title = "THIWN THAH HUB",
+        Description = "FREEMIUM V1",
         KeyLink = "https://sub4unlock.io/AarF9",
         Keys = {"KEY_27032025", "KEY_25023072", "KEY_27033072"},
         Notifi = {
         Notifications = true,
-        CorrectKey = "Running the Script...",
-       Incorrectkey = "The key is incorrect",
-       CopyKeyLink = "Copied to Clipboard"
+        CorrectKey = "ĐANG CHẠY SCRIPT",
+       Incorrectkey = "KEY KHÔNG ĐÚNG",
+       CopyKeyLink = "ĐÃ SAO CHÉP LINK"
       }
     }
   })
 
        MinimizeButton({
        Image = "http://www.roblox.com/asset/?id=88562990432140",
-       Size = {50, 50},
+       Size = {40, 40},
        Color = Color3.fromRGB(255, 255, 255),
        Corner = true,
        Stroke = true,
-       StrokeColor = Color3.fromRGB(255, 0, 0)
+       StrokeColor = Color3.fromRGB(0, 0, 0)
       })
       
 ------ Tab
