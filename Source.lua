2c90a4166961ebc8209898398a845b297ab5a751
thiwnthah <phamngocthienthanh207@gmail.com>
2025-03-27 19:02:59 +0700

    Source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/thiwnthah/BloxFruit/refs/heads/main/UI.lua")))()
 
        local Window = MakeWindow({
          Hub = {
