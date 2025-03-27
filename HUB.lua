3ea2304c8f3e7147ed94aa38d3727252e2b80195
thiwnthah <phamngocthienthanh207@gmail.com>
2025-03-27 19:11:38 +0700

    Add files via upload

---
+++ HUB.lua
@@ -0,0 +1,47 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/thiwnthah/BloxFruit/refs/heads/main/UI.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "THIWN THAH HUB",
+         Animation = "THIWN THAH HUB"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "THIWN THAH HUB",
+        Description = "FREEMIUM V1",
+        KeyLink = "https://sub4unlock.io/AarF9",
+        Keys = {"KEY_27032025", "KEY_25023072", "KEY_27033072"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "DANG CHAY SCRIPT",
+       Incorrectkey = "KEY KHONG DUNG",
+       CopyKeyLink = "DA SAO CHEP LINK"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=88562990432140",
+       Size = {40, 40},
+       Color = Color3.fromRGB(255, 255, 255),
+       Corner = true,
+       Stroke = true,
+       StrokeColor = Color3.fromRGB(0, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "SCRIPT"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "REDZ HUB",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+  end
+  })
