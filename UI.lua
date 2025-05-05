4792955d27b13cf897e51b00f58b8c1006fa4eb7
thiwnthah <phamngocthienthanh207@gmail.com>
2025-05-05 20:06:11 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -16,7 +16,7 @@ local redzlib = {
 				ColorSequenceKeypoint.new(0.50, Color3.fromRGB(255, 255, 255)),
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(255, 255, 255))
 			}),
-			["Color Hub 2"] = Color3.fromRGB(200, 200, 200),
+			["Color Hub 2"] = Color3.fromRGB(250, 250, 250),
 			["Color Stroke"] = Color3.fromRGB(0, 0, 0),
 			["Color Theme"] = Color3.fromRGB(200, 200, 200),
 			["Color Text"] = Color3.fromRGB(0, 0, 0),
